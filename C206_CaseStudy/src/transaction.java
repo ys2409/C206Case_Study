7d7a72e4131655740ee691077d1dcd268af2336c
Izwan <20011187@myrp.edu.sg>
2021-08-10 01:57:54 +0800

    transaction class

---
+++ C206_CaseStudy/src/transaction.java
@@ -0,0 +1,31 @@
+
+public class transaction {
+		private double amount;
+		private double received;
+		private String currency;
+		private String converted;
+		private String date;
+		public transaction(String date, String currency, double amount,String converted, double received) {
+			this.date = date;
+			this.currency = currency;
+			this.amount = amount;
+			this.received = received;
+			this.converted = converted;
+		}
+		public String getDate() {
+			return date;
+		}
+		public String currency() {
+			return currency;
+		}
+		public double getAmount() {
+			return amount;
+		}
+		public String getConverted() {
+			return converted;
+		}
+		public double received() {
+			return received;
+		}
+
+}
